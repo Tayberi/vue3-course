ee5573bf9c053b5086741df1aa88deb7eee6e73d
Tayberi Evgeny <tayberi00@gmail.com>
2023-02-06 11:42:10 +0500

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,116 @@
-<template>
-  <div>
-    <div>Кол - во лайков {{ likes }}</div>
+<template xmlns="http://www.w3.org/1999/html">
+  <div class="app">
+    <h1>Страница с постами</h1>
+
+    <div class="app__btns">
+      <my-button
+          @click="showDialog"
+      >
+        Создать пользователя
+      </my-button>
+      <my-select
+        v-model="selectedSort"
+        :options="sortOptions"
+      >
+
+      </my-select>
+    </div>
+
+    <my-dialog v-model:show="dialogVisible">
+      <post-form
+          @create="createPost"
+      />
+    </my-dialog>
+
+    <post-list
+        :posts="sortedPosts"
+        @remove="removePost"
+        v-if="!isPostLoading"
+    />
+    <div v-else>Идет загрузка ...</div>
   </div>
 </template>
 
 <script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+import MyButton from "@/components/UI/MyButton";
+import  axios from 'axios'
+import MySelect from "@/components/UI/MySelect";
 export default {
+  components: {
+    MySelect,
+    MyButton,
+    PostList,
+    PostForm
+  },
   data() {
     return {
-      likes: 0,
+      posts: [],
+      dialogVisible: false,
+      isPostLoading: false,
+      selectedSort: '',
+      sortOptions: [
+        {value: 'title', name: 'По названию'},
+        {value: 'body', name: 'По содержимому'},
+      ]
     }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+      this.dialogVisible = false;
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id)
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
+    async fetchPosts() {
+      try {
+        this.isPostLoading = true;
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5');
+        this.posts = response.data;
+      } catch (e) {
+        alert('Ошибка');
+      } finally {
+        this.isPostLoading = false;
+      }
+    }
+  },
+  mounted() {
+    this.fetchPosts();
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+    }
+  },
+  watch: {
+    // selectedSort(newValue) {
+    //   this.posts.sort((post1, post2) => {
+    //     return post1[newValue]?.localeCompare(post2[newValue])
+    //   })
+    // },
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+.app {
+  padding: 20px;
+}
+
+.app__btns {
+  margin:  15px 0;
+  display: flex;
+  justify-content: space-between;
+}
 </style>
